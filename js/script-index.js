7cf799ad29546fec695169d59e79c2a7ace29460
isjulia211 <109786593+isjulia211@users.noreply.github.com>
2023-08-25 13:08:41 +1000

    about page

--- js/script-index.js
+++ js/script-index.js
@@ -48,3 +48,18 @@ window.addEventListener("scroll", () => {
     });
   });
 });
+//number increasing
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 7000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 10;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
